6104afed1e4ec31287b3920342fc6d74d8d11558
lucas_thielmann <lucas.thielmann@hpe.com>
2016-08-02 08:46:55 +0200

    add "getName" and "remove" operations to closer approximate the calls made in production code

--- src/main/java/com/ditemis/logic/TestNodeManager.java
+++ src/main/java/com/ditemis/logic/TestNodeManager.java
@@ -1,16 +1,14 @@
 package com.ditemis.logic;
 
-import java.util.UUID;
+import com.ditemis.rs.TestService;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
-
-import org.jboss.logging.Logger;
-
-import com.ditemis.rs.TestService;
+import java.util.UUID;
 
 @RequestScoped
 public class TestNodeManager {
@@ -21,30 +19,31 @@ public class TestNodeManager {
     private Session session;
     
     public String buildTestNode() throws RepositoryException {
-        LOGGER.info("buildTestNode");
+//        LOGGER.info("buildTestNode");
         try {
-            Node rootNode = session.getRootNode();
-            Node newNode = rootNode.addNode("test/" + UUID.randomUUID().toString());
+            Node testNode = session.getRootNode().getNode("test");
+            Node newNode = testNode.addNode(UUID.randomUUID().toString());
             newNode.setProperty("status", "new");
             
             Node childNode1 = newNode.addNode("child1");
             childNode1.setProperty("test", "test");
-            
+
             Node childNode2 = newNode.addNode("child2");
             childNode2.setProperty("test", "test");
-            
+
             Node childNode3 = newNode.addNode("child3");
             childNode3.setProperty("test", "test");
-            
+
             Node childNode4 = newNode.addNode("child4");
             childNode4.setProperty("test", "test");
-            
+
             Node childNode5 = newNode.addNode("child5");
             childNode5.setProperty("test", "test");
-            
+
             session.save();
-            
+
             Node getNewNode = session.getNodeByIdentifier(newNode.getIdentifier());
+            getNewNode.getName();
             return getNewNode.getIdentifier();
         } catch (RepositoryException rex) {
             LOGGER.info("Failed building new node", rex);
@@ -53,11 +52,15 @@ public class TestNodeManager {
     }
     
     public String updateTestNode(String nodeId, String status) throws RepositoryException {
-        LOGGER.info("updateTestNode " + nodeId);
+//        LOGGER.info("updateTestNode " + nodeId);
         try {
             Node testNode = session.getNodeByIdentifier(nodeId);
             testNode.setProperty("status", status);
-            
+
+            testNode.getNode("child1").remove();
+            Node newChildNode = testNode.addNode("child1");
+            newChildNode.setProperty("test", status);
+
             session.save();
             
             return testNode.getIdentifier();
--- src/main/java/com/ditemis/rs/TestService.java
+++ src/main/java/com/ditemis/rs/TestService.java
@@ -1,16 +1,12 @@
 package com.ditemis.rs;
 
 
+import com.ditemis.logic.TestNodeManager;
+
 import javax.inject.Inject;
 import javax.jcr.RepositoryException;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-
-import com.ditemis.logic.TestNodeManager;
 
 @Path("/test")
 public class TestService {
